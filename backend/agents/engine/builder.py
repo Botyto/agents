acc562e81226f21f8d70906eafe88664e0b369f4
biser <bisserkr@gmail.com>
2024-07-21 09:20:06 +0300

    Custom agent builder can add capabilities

--- backend/agents/engine/builder.py
+++ backend/agents/engine/builder.py
@@ -1,4 +1,5 @@
-from typing import List, Optional, Union
+from typing import Any, Dict, List, Optional, Tuple, Type, Union
+from autogen import ConversableAgent
 from autogen.agentchat.contrib.agent_builder import AgentBuilder
 from autogen.agentchat.contrib.capabilities.agent_capability import AgentCapability
 
@@ -27,3 +28,70 @@ class AgentWithCapabilitiesBuilder(AgentBuilder):
             max_agents=max_agents,
         )
         self.capabilities = capabilities or []
+
+    AGENT_CAPABILITIES_PROMPT = """# Your goal
+Considering the following task, what capabilities should the following expert have (if any).
+
+# Expert name
+{name}
+
+# Available capabilities
+{capabilities}
+
+# Task requirement
+- Only reply the names of the capabilities, separated by ",".
+For example: YoutubeCapability, WebSurferCapability, ... """
+
+    def _build_agent_capabilities(self, config: Dict[str, Any]):
+        print(f"Preparing capabilities for {config['name']}", flush=True)
+        capabilities_text = ", ".join(
+            cap.__class__.__name__
+            for cap in self.capabilities
+        )
+        resp_agent_capabilities = (
+            self.builder_model.create(
+                messages=[
+                    {
+                        "role": "user",
+                        "content": self.AGENT_CAPABILITIES_PROMPT.format(**config, capabilities=capabilities_text),
+                    }
+                ]
+            )
+            .choices[0]
+            .message.content
+        )
+        capabilities_list = [
+            agent_name.strip()
+            for agent_name in resp_agent_capabilities.split(",")
+        ]
+        capabilities_list = [
+            cap
+            for cap in self.capabilities
+            if cap.__class__.__name__ in capabilities_list
+        ]
+        return capabilities_list
+
+    def _agent_expand_config(self, config: Dict[str, Any]) -> Dict[str, Any]:
+        config["capabilities"] = self._build_agent_capabilities(config)
+        super()._agent_expand_config(config)
+
+    def _build_agents(
+        self, use_oai_assistant: Optional[bool] = False, user_proxy: Optional[ConversableAgent] = None, **kwargs
+    ) -> Tuple[List[ConversableAgent], Dict]:
+        agent_list, cached_configs = super()._build_agents(
+            use_oai_assistant=use_oai_assistant, user_proxy=user_proxy, **kwargs
+        )
+        for agent, config in zip(agent_list, cached_configs):
+            if not config["capabilities"]:
+                continue
+            for cap_class_name in config["capabilities"]:
+                cap_class: Type[AgentCapability]|None = next(
+                    cap
+                    for cap in self.capabilities
+                    if cap.__class__.__name__ == cap_class_name
+                )
+                if cap_class is None:
+                    continue
+                cap = cap_class()
+                cap.add_to_agent(agent)
+        return agent_list, cached_configs
